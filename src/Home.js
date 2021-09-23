e62cad7530256d9b8435bef4ffbeaf7f4dd8622d
Genevieve <vievenavales29@gmail.com>
2021-09-24 01:12:53 +0800

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello, ninjas", e.target);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`hello, ${name}`);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
     </div>
   );
 };
