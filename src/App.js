648ab4f5773d78956f02f810408aa8e74e6ae87e
Genevieve <vievenavales29@gmail.com>
2021-09-24 17:24:46 +0800

    handling fetch errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { useState } from "react/cjs/react.development";
 
 function App() {
   return (
--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,31 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response) {
+          throw Error("could not fetch data for that resource");
+        }
+
+        return response.json();
+      })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((e) => {
+        setIsPending(false);
+        setError(e.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
